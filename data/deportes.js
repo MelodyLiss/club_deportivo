7c7c8a001ed83282b5e96dc5b9cac4b44ad41288
MelodyLiss <lisette.jofre.1@gmail.com>
2024-10-03 09:29:39 -0300

    estructura de carpetas

---
+++ data/deportes.js
@@ -0,0 +1,13 @@
+[
+    {
+        "id" : "1",
+        "deporte": "Karate",
+        "precio": 25000
+    },
+
+    {
+        "id" : "2",
+        "deporte": "Boxeo",
+        "precio": 35000
+    },
+]
